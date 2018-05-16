6bb098091ac942d46fd27a0285e59e191a7622e2
kristyyang1110 <kristyyang1110@gmail.com>
2018-05-16 12:28:37 -0700

    new pages

--- Practice projects/voting_app/public/js/app.js
+++ Practice projects/voting_app/public/js/app.js
@@ -1,1 +1,8 @@
-
+class ProductList extends React.Component {
+  render() {
+    return (
+      <div className='ui unstackable items'>
+        Hello, friend! I am a basic React component.
+      </div>
+); }
+}
